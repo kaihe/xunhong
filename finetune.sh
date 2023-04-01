87dde935d0c9f7c3fc8ed873da39e89bfce9740c
kaihe-msi <xukaihenupt@gmail.com>
2023-04-01 17:45:05 +0800

    update fin.sh

--- finetune.sh
+++ finetune.sh
@@ -1,10 +1,10 @@
-TOT_CUDA="0"
+TOT_CUDA="0,1,2,3,4,5,6,7"
 CUDAs=(${TOT_CUDA//,/ })
 CUDA_NUM=${#CUDAs[@]}
 PORT="1234"
 
-DATA_PATH="./sample/merge_sample.json"
-OUTPUT_PATH="lora-Vicuna"
+DATA_PATH="./train_data/belle_metra_diago.json"
+OUTPUT_PATH="./adapters/belle+metra"
 MODEL_PATH="decapoda-research/llama-7b-hf"
 
 CUDA_VISIBLE_DEVICES=${TOT_CUDA} torchrun --nproc_per_node=$CUDA_NUM --master_port=$PORT finetune.py \
